33e84f14773c866197a71deed5e8a51b1938f39a
Saud Alshamsi <23ud92@gmail.com>
2019-10-28 21:22:17 +0300

    finished part 1

--- src/FilmDetails.js
+++ src/FilmDetails.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 
 class FilmDetails extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {}
-    }
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {}
+    // }
     render() {
         return (
             <div className="film-details">
