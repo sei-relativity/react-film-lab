200bf380cc4908efd666437bf43d7190edc14eec
mansourSaleh <mansouralmohsen2018@gmai.com>
2019-10-29 11:20:15 +0300

    Part2

---
+++ src/Fave.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+
+class Fave extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      isFave: false
+    }
+  }
+
+  handleClick = (e)=> {
+    e.stopPropagation()
+    console.log("click")
+    this.setState({isFave: !this.state.isFave})
+  }
+  render() {
+    const isFave = this.state.isFave ? 'remove_from_queue' : 'add_to_queue'
+    const message = this.state.isFave ? "remove_from_queue" : "add_to_queue"
+    return (
+      <div className={`film-row-fave ${isFave}`} onClick={this.handleClick}>
+        <p className="material-icons">{message}</p>
+      </div>
+    );
+  }
+}
+
+export default Fave;
--- src/FilmListing.js
+++ src/FilmListing.js
@@ -2,6 +2,18 @@ import React, { Component } from "react";
 import FilmRow from "./FilmRow";
 
 class FilmListing extends Component {
+
+constructor(props){
+  super(props)
+  this.state = {
+    filter: 'all'
+  }
+}
+
+  handleFilterClick = (filter)=>{
+    this.setState({filter: filter})
+  }
+
   render() {
     const allFilms = this.props.movies.films.map((item, index) => {
         return <FilmRow film={item} key={index}/>
@@ -9,10 +21,21 @@ class FilmListing extends Component {
 
 
     return (
-      <div className="film-list">
-        <h1 className="section-title">FILMS</h1>
-        {allFilms}
-      </div>
+     <div className="film-list">
+    <h1 className="section-title">FILMS</h1>
+    <div className="film-list-filters">
+        <div onClick={() => this.handleFilterClick('all')}  className={`film-list-filter ${this.state.filter === 'all' ? 'is-active' : ''}`}>
+            ALL
+            <span className="section-count">{this.props.movies.films.length}</span>
+        </div>
+        <div onClick={() => this.handleFilterClick('faves')} className={`film-list-filter ${this.state.filter === 'faves' ? 'is-active' : ''}`}>
+            FAVES
+            <span className="section-count">0</span>
+        </div>
+    </div>
+
+    {allFilms}
+</div>
     );
   }
 }
