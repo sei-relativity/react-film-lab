3568a5a1757ccebba81d2a40e13e13ae069a3ddb
Hisham Aljahbli <h.aljahbli@gmail.com>
2019-10-29 20:57:11 +0300

    compleated hw day 2

--- src/FilmListing.js
+++ src/FilmListing.js
@@ -4,17 +4,24 @@ import FilmRow from './FilmRow'
 
 class FilmListing extends Component {
 
+    constructor(props){
+        super (props)
+
+        // initial state
+        this.state = {
+            filter: 'all'
+        }
+    }
+
     handleFilterClick = (filter) => {
         console.log(`Setting filter to ${filter}`);
+        this.setState({
+            filter:filter
+        })
     };
 
-
-    
-
-    
-
-
     render(){
+
 
         const allFilms = this.props.movies.films.map((movie, index) => {
             return <FilmRow film={movie} key={index}></FilmRow>
@@ -23,13 +30,14 @@ class FilmListing extends Component {
         return (
             <div className="film-list">
                 <h1 className="section-title">FILMS</h1>
+
                 <div className="film-list-filters">
-                    <div className="film-list-filter" onClick={() => this.handleFilterClick('all')}>
+                <div className={`film-list-filter ${this.state.filter === 'all' ? 'is-active' : ''}`} onClick={() => this.handleFilterClick('all')}>
                         ALL
                         <span className="section-count">{this.props.movies.length}</span>
                     </div>
 
-                    <div className="film-list-filter" onClick={() => this.handleFilterClick('faves')}>
+                    <div className={`film-list-filter ${this.state.filter === 'faves' ? 'is-active' : ''}`} onClick={() => this.handleFilterClick('faves')}>
                         FAVES
                         <span className="section-count">0</span>
                     </div>
