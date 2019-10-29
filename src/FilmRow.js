c41d4d4a66189fcc271492ec5792c4e72a98d169
saraOmar18 <sezla18@gmail.com>
2019-10-29 14:32:38 +0300

    Sara Homework

---
+++ src/FilmRow.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react' ; 
+
+class FilmRow extends Component {
+    render () {
+     
+       return (
+           <div className='film-row'>
+        <img src={"https://image.tmdb.org/t/p/w780/"+this.props.film.poster_path} alt="" />
+        <div className="film-summary">
+           <h1>{this.props.film.title}</h1>
+           <p>{this.props.film['release_date']}</p>
+        </div> </div>
+       )
+        }
+}   
+
+
+export default FilmRow ; 
