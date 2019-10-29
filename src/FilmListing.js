9ec79bf0e53d65c7e1c4fd5f3c63c292f613776f
Hisham Aljahbli <h.aljahbli@gmail.com>
2019-10-29 19:26:19 +0300

    part 2 hw day 2

--- src/FilmListing.js
+++ src/FilmListing.js
@@ -3,6 +3,17 @@ import FilmRow from './FilmRow'
 
 
 class FilmListing extends Component {
+
+    handleFilterClick = (filter) => {
+        console.log(`Setting filter to ${filter}`);
+    };
+
+
+    
+
+    
+
+
     render(){
 
         const allFilms = this.props.movies.films.map((movie, index) => {
@@ -10,10 +21,21 @@ class FilmListing extends Component {
         })
 
         return (
-        <div className="film-list">
-            <h1 className="section-title">FILMS</h1>
-            {allFilms}
-        </div>
+            <div className="film-list">
+                <h1 className="section-title">FILMS</h1>
+                <div className="film-list-filters">
+                    <div className="film-list-filter" onClick={() => this.handleFilterClick('all')}>
+                        ALL
+                        <span className="section-count">{this.props.movies.length}</span>
+                    </div>
+
+                    <div className="film-list-filter" onClick={() => this.handleFilterClick('faves')}>
+                        FAVES
+                        <span className="section-count">0</span>
+                    </div>
+                </div>
+                {allFilms}
+            </div>
         )
     }
 }
