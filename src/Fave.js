8a81c52346f50bc8907d0d5a73bb3970dcb81225
Hisham Aljahbli <h.aljahbli@gmail.com>
2019-10-29 18:57:09 +0300

    finished task 1 of hw 2

---
+++ src/Fave.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+
+class Fave extends Component{
+
+    handleClick =(e) => {
+        console.log("handling Fave click!");
+        
+    }
+
+    render(){
+        return (
+            <div className="film-row-fave add_to_queue">
+                <p className="material-icons" onClick={this.handleClick}>add_to_queue</p>
+            </div>
+        )
+    }
+}
+
+export default Fave;
--- src/FilmRow.js
+++ src/FilmRow.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
-import FilmPoster from './FilmPoster'
+import FilmPoster from './FilmPoster';
+import Fave from './Fave';
+
 
 
 class FilmRow extends Component {
@@ -7,11 +9,13 @@ class FilmRow extends Component {
     render(){
         return(
             <div className="film-row">
-                <FilmPoster img={this.props.film.poster_path}></FilmPoster>
+                <FilmPoster img={this.props.film.poster_path} />
+
                 <div className="film-summary">
                     <h1>{this.props.film.title}</h1>
                     <p>{new Date(this.props.film.release_date).getFullYear()}</p>
                 </div>
+                <Fave/>
             </div>
         )
     }
