a4d9dbbef1293077b6f30b3e33d5ee35f9629cf9
Hisham Aljahbli <h.aljahbli@gmail.com>
2019-10-29 20:28:37 +0300

    part 4 hw day 2 done

--- src/Fave.js
+++ src/Fave.js
@@ -2,15 +2,29 @@ import React, {Component} from 'react';
 
 class Fave extends Component{
 
+    constructor(props){
+        super (props)
+
+        // initial state
+        this.state = {
+            isFave: false
+        }
+    }
+
     handleClick =(e) => {
         console.log("handling Fave click!");
         e.stopPropagation()
+        this.setState ({
+            isFave: !this.state.isFave
+        })
     }
 
     render(){
+
+        const isFave = (this.state.isFave) ? 'remove_from_queue' : 'add_to_queue';
         return (
-            <div className="film-row-fave add_to_queue">
-                <p className="material-icons" onClick={this.handleClick}>add_to_queue</p>
+            <div className={`film-row-fave ${isFave}`}>
+                <p className="material-icons" onClick={this.handleClick} >{isFave}</p>
             </div>
         )
     }
