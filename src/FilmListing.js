3b661498787ae27d62913abd33825fca307188f7
AlShabibi <shabams@gmail.com>
2019-10-31 00:11:09 +0300

    done with part-1 and doing part-2

--- src/FilmListing.js
+++ src/FilmListing.js
@@ -2,6 +2,9 @@ import React, { Component } from "react";
 import FilmRow from "./FilmRow";
 
 class FilmListing extends Component {
+  handleFilterClick = string => {
+    console.log("Setting filter to ", string);
+  };
   render() {
     const allFilms = this.props.db.films.map((item, key) => {
       return (
@@ -16,6 +19,17 @@ class FilmListing extends Component {
     return (
       <div className="film-list">
         <h1 className="section-title">FILMS</h1>
+        <div className="film-list-filters">
+          <div className="film-list-filter">
+            ALL
+            <span className="section-count">{this.props.films.length}</span>
+          </div>
+          <div className="film-list-filter">
+            FAVES
+            <span className="section-count">0</span>
+          </div>
+        </div>
+
         {allFilms}
       </div>
     );
--- src/FilmRow.js
+++ src/FilmRow.js
@@ -1,21 +1,21 @@
 import React, { Component } from "react";
+import Fave from "./Fave";
 
 class FilmRow extends Component {
   render() {
-    var moonLanding = new Date(this.props.year);
+    const theYear = new Date(this.props.year);
     return (
       <div className="film-row">
         <img
-          src={{
-            uri: "https://image.tmdb.org/t/p/w780/" + this.props.poster_path
-          }}
-          alt=""
+          src={`https://image.tmdb.org/t/p/w780${this.props.postUtl}`}
+          alt="Movies Images"
         />
 
         <div className="film-summary">
           <h1>{this.props.title}</h1>
-          <p>{moonLanding.getFullYear(this.props.year)}</p>
+          <p>{theYear.getFullYear()}</p>
         </div>
+        <Fave />
       </div>
     );
   }
