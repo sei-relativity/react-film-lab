da95c76bc5b5050cdd41e267e39d3acc5b646c70
YOUR GITHUB nawal <YOUR GITHUB nawaldee21@gmail.com>
2019-11-15 17:27:48 +0300

    nawal

--- src/FilmListing.js
+++ src/FilmListing.js
@@ -1,18 +1,48 @@
-import React, {Component} from 'react';
-import FilmRow from './FilmRow';
-class FilmListing extends Component{
-    render(){
-    const allFilms = this.props.movies.films.map((item,index) => {
-    return( 
-    <FilmRow film={item} key={index} /> 
-         ); } );
- return(
-<div className="film-list">
-    <h1 className="section-title">FILMS</h1>
-   {allFilms}
-  </div>
- );
-    
+import React, { Component } from "react";
+import FilmRow from "./FilmRow";
+
+class FilmListing extends Component {
+
+constructor(props){
+  super(props)
+  this.state = {
+    filter: 'all'
+  }
 }
+
+  handleFilterClick = (filter)=>{
+    this.setState({filter: filter})
+  }
+
+  render() {
+
+
+    let showFilms = [];
+    const allFilms = this.props.movies.films.map((item, index) => {
+        return <FilmRow handleDetailsClick={this.props.handleDetailsClick} film={item} key={index} isFave={this.props.faves.includes(item)} onFaveToggle={() =>this.props.onFaveToggle(item)}/>
+    })
+    const favesFilms = this.props.faves.map((item, index) => {
+      return <FilmRow handleDetailsClick={this.props.handleDetailsClick} film={item} key={index} isFave={this.props.faves.includes(item)} onFaveToggle={() =>this.props.onFaveToggle(item)}/>
+    })
+   this.state.filter === 'all' ? showFilms = allFilms : showFilms = favesFilms
+
+    return (
+     <div className="film-list">
+    <h1 className="section-title">FILMS</h1>
+    <div className="film-list-filters">
+        <div onClick={() => this.handleFilterClick('all')}  className={`film-list-filter ${this.state.filter === 'all' ? 'is-active' : ''}`}>
+            ALL
+            <span className="section-count">{this.props.movies.films.length}</span>
+        </div>
+        <div onClick={() => this.handleFilterClick('faves')} className={`film-list-filter ${this.state.filter === 'faves' ? 'is-active' : ''}`}>
+            FAVES
+            <span className="section-count">{this.props.faves.length}</span>
+        </div>
+    </div>
+
+    {showFilms}
+</div>
+    );
+  }
 }
 export default FilmListing;
